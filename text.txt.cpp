ad75e36ad27ef8738f5193cbc71958b0be671199
woahdas <78304648+woahdas@users.noreply.github.com>
2021-06-05 17:22:15 +0100

    Create text.txt.cpp

---
+++ text.txt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+int ACC = 0; //accumulator
+int BAK = 0; //backup
+int NIL = 0; //nil value
+if (ACC > 0){
+};
+return 0;
+//evidence that it works (done by converting JGZ (Jump if Greater Than Zero) to CPP using the converter file)
+}
